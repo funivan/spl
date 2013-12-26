2c814bb4ab3e3fc6744fc9f4a334dc04971c54fa
Funivan <dev@funivan.com>
2013-12-26 19:26:04 +0200

    Add string object tests

--- tests/Tests/Fiv/Spl/StringTest.php
+++ tests/Tests/Fiv/Spl/StringTest.php
@@ -25,6 +25,34 @@
       $string = new String('test-123');
       $string->regexReplace('!-\d+!', '-test');
       $this->assertEquals('test-test', (string)$string);
+
+      $string = new String('test-124');
+      $string->regexReplace(array(
+        '![a-z]+!' => '_',
+        '![\d]+!' => '/',
+      ));
+      $this->assertEquals('_-/', (string)$string);
+    }
+
+    public function testRegexContain() {
+      $string = new String('test-123');
+      $result = $string->regexContain('!-\d+!');
+      $this->assertTrue($result);
+
+      $result = $string->regexContain('!f\d!');
+      $this->assertFalse($result);
+
+      $string = new String('test-124');
+      $result = $string->regexContain(array(
+        '![a-z]+!',
+        '![\d]+!',
+      ));
+      $this->assertTrue($result);
+      $result = $string->regexContain(array(
+        '!md\d!',
+        '!cc[a-z]+!',
+      ));
+      $this->assertFalse($result);
     }
 
   }
